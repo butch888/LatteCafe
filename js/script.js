9bb166f822188d1c093aa7f6aaace68454a2eee2
Vlad <90765364+butch888@users.noreply.github.com>
2022-09-05 22:26:56 +0300

    LatteCafe

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// indexe.html строка 65
+function onClickButton1() {
+    document.getElementById('menuImgId').src="./images/menu-img1.jpeg";
+}
+
+// indexe.html строка 66
+function onClickButton2() {
+    document.getElementById('menuImgId').src="./images/menu-img2.jpeg";
+}
+
+// indexe.html строка 67
+function onClickButton3() {
+    document.getElementById('menuImgId').src="./images/menu-img3.jpeg";
+}
+
